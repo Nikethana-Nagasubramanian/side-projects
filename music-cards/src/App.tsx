dee2b9b7cdf80488939a93d7448b0ad00ce92654
nike935 <nike@growthfactor.ai>
2025-05-14 14:04:18 -0400

    Committing music-cards base folder

---
+++ music-cards/src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="text-center">
+      <header className="bg-gray-800 min-h-screen flex flex-col items-center justify-center text-white text-2xl">
+        <p className="mb-4">
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="text-blue-400"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <p className="mt-4 text-lg text-green-400 font-bold">
+          Tailwind CSS is working!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App; 
